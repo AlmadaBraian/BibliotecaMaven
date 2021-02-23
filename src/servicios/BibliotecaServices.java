0ecc0cf983e1bce6ac0cb43649a3c334b30a3766
SpoilerFest <almadabraian93@gmail.com>
2021-02-23 18:15:27 -0300

    se modifico BibliotecaServices

--- src/servicios/BibliotecaServices.java
+++ src/servicios/BibliotecaServices.java
@@ -7,10 +7,12 @@ import jakarta.jws.WebParam;
 import jakarta.jws.WebService;
 import mavenProyecto.Biblioteca;
 import mavenProyecto.Copia;
+import mavenProyecto.Lector;
 import mavenProyecto.Libro;
 import mavenProyecto.estadoCopia;
 import persistencia.BibliotecaDAO;
 import persistencia.CopiaDAO;
+import persistencia.LectorDAO;
 
 @WebService
 public class BibliotecaServices {
@@ -34,5 +36,18 @@ public class BibliotecaServices {
 		BibliotecaDAO dao = new BibliotecaDAO();
 		dao.modEstadoCopia(copia, e);
 	}
+	
+	@WebMethod(operationName="obtenerLectorWS")
+	public Lector obtenerLector(@WebParam(name="lectorID")long id) {
+		LectorDAO ldao = new LectorDAO();
+		return ldao.obtenerLector(id);
+	}
+	
+	@WebMethod(operationName="obtenerCopiaWS")
+	public Libro obtenerCopia(@WebParam(name="lectorID")long id) {
+		CopiaDAO cdao = new CopiaDAO();
+		return cdao.obtenerCopia(id);
+	}
+
 
 }
